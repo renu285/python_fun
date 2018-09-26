fc8d9d1d3b1b95d71aaaaa5b0dc6bfed875934f3
renu <renu285@gmail.com>
2018-09-26 15:30:53 +0200

    Added otal amount

--- parser.py
+++ parser.py
@@ -13,12 +13,13 @@ class DataParser(object):
             rawsource = vals[7]
             rawsource = " ".join(rawsource.split())
             if(rawsource.split(' ')[0]== 'BEA'):
+                self.amountTotal += amount
                 print amount, rawsource.split(' ')[3]
-
+        print "Total Amount spent : ", self.amountTotal 
 
     def __init__(self,dataFile):
         self.source = []
-        self.amount = []
+        self.amountTotal = 0.0 
         self.expense = []
         self.lines = [] 
         fp = open(dataFile,'r')
