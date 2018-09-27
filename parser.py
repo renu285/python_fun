16682cfb0df7c46090c4dfb829722f55bc0c63e4
renu <renu285@gmail.com>
2018-09-27 15:48:52 +0200

    Updated parser

--- parser.py
+++ parser.py
@@ -13,11 +13,46 @@ class DataParser(object):
             rawsource = vals[7]
             rawsource = " ".join(rawsource.split())
             if(rawsource.split(' ')[0]== 'BEA'):
+                dateTime=rawsource.split(" ")[2]
+                date=dateTime.split("/")[0]
+                time=dateTime.split("/")[1]
+                rawsource = rawsource.split("/")[1]
+                source = rawsource.split(",")[0]
+                source = source.split(None,1)[1]
+                source = ' '.join(source.split()[:2])
                 amount *= -1
                 self.amountTotal += amount
-                print amount, rawsource.split(' ')[3]
+                print "%4.2f\t%6s\t%6s\t%s" %(amount, date,time, source)
             else :
-                print amount, rawsource
+                try:
+                    name_index = rawsource.index("NAME")
+                    dateTime=rawsource.split("/")[12]
+                    rawsource = rawsource[name_index:]
+                    source = rawsource.split("/")[1]
+                    if(amount < 0) :
+                        amount *= -1
+                        self.amountTotal += amount
+                    try:
+                        date = dateTime.split(" ")[0]
+                        time = dateTime.split(" ")[1]
+                    except IndexError:
+                        date = " "
+                        time = " " 
+                    print "%4.2f\t%6s\t%6s\t%s" %(amount,date,time, source)
+
+                except ValueError:
+                    try:
+                        name_index = rawsource.index("Naam")
+                        rawsource = rawsource[name_index:]
+                        source = rawsource.split(":")[1]
+                        if(amount < 0) :
+                            amount *= -1
+                            self.amountTotal += amount
+                            print "%4.2f\t%6s\t%6s\t%s" %(amount, '    ','     ', source)
+                    except ValueError:
+                        if(amount < 0) :
+                            amount *= -1
+                            self.amountTotal += amount
         print "Total Amount spent : ", self.amountTotal 
 
     def __init__(self,dataFile):
